dcdeffa882c5388792c195b1b6e6c1bb019301b8
Anusha <20cs015.anusha@sjec.ac.in>
2024-04-02 02:13:25 +0530

    Error with the about page circles

--- website/src/Pages/About.jsx
+++ website/src/Pages/About.jsx
@@ -18,20 +18,22 @@ const About = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </h1>
 
-         <div className="buttoncontainer">
+         <div className="buttoncontainer" onClick={handleClick}>
           <button id="book-now-button" onClick={handleClick}>
             Learn More
           </button>
         </div>
 
+        {/* <div className="sideimage">
+            <img className="circle" src={sideimage}/>
+        </div> */}
+
+
         <div className="image">
             <img className="anish" src={about}/>
         </div>
 
-        <div className="sideimage">
-            <img className="circle" src={sideimage}/>
-        </div>
-
+        
        
         
 
